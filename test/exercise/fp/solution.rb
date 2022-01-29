775571f4ca473cbb90510985f2c4ce19edc7c4dd
Aynur Galimov <anrgl@ya.ru>
2022-01-29 11:30:00 +0500

    Implement fp module

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,19 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        films = array.reject { |f| f['country'].nil? }
+        films = films.select { |f| f['country'].split(',').size > 1 }
+        films = films.reject { |f| f['rating_kinopoisk'].nil? }
+        films = films.reject { |f| f['rating_kinopoisk'] == '0' }
+        total = films.reduce(0) { |sum, f| sum + f['rating_kinopoisk'].to_f }
+        total / films.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        kp_films = films.reject { |f| f['rating_kinopoisk'].nil? }
+        kp_films = kp_films.select { |f| f['rating_kinopoisk'].to_f >= threshold }
+        kp_films.reduce(0) { |sum, f| sum + f['name'].count('и') }
       end
     end
   end
